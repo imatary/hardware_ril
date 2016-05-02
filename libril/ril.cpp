fe341f665dfb9d9a2ad0e90e862059fab05a218e
kevin <huhuikevin@gmail.com>
2016-05-02 15:40:51 +0800

    add unsol msg debug
    Signed-off-by: kevin <huhuikevin@gmail.com>

--- libril/ril.cpp
+++ libril/ril.cpp
@@ -87,7 +87,7 @@ namespace android {
 #define PRINTBUF_SIZE 8096
 
 // Enable RILC log
-#define RILC_LOG 0
+#define RILC_LOG 1
 
 #if RILC_LOG
     #define startRequest           sprintf(printBuf, "(")
@@ -1037,7 +1037,7 @@ dispatchImsGsmSms(Parcel &p, RequestInfo *pRI, uint8_t retry, int32_t messageRef
     rism.messageRef = messageRef;
 
     startRequest;
-    appendPrintBuf("%sformat=%d,", printBuf, rism.format);
+    appendPrintBuf("%sformat=%d,", printBuf, rism.messageRef);
     if (countStrings == 0) {
         // just some non-null pointer
         pStrings = (char **)alloca(sizeof(char *));
@@ -1482,7 +1482,7 @@ static void dispatchSetInitialAttachApn(Parcel &p, RequestInfo *pRI)
 
     startRequest;
     appendPrintBuf("%sapn=%s, protocol=%s, auth_type=%d, username=%s, password=%s",
-            printBuf, pf.apn, pf.protocol, pf.auth_type, pf.username, pf.password);
+            printBuf, pf.apn, pf.protocol, pf.authtype, pf.username, pf.password);
     closeRequest;
     printRequest(pRI->token, pRI->pCI->requestNumber);
 
@@ -3508,6 +3508,7 @@ void RIL_onUnsolicitedResponse(int unsolResponse, void *data,
     bool shouldScheduleTimeout = false;
     RIL_RadioState newState;
 
+	RLOGE("[UNSL]---------unsolResponse=%d, %x", unsolResponse, unsolResponse);
     if (s_registerCalled == 0) {
         // Ignore RIL_onUnsolicitedResponse before RIL_register
         RLOGW("RIL_onUnsolicitedResponse called before RIL_register");
